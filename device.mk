ca92f02bcf5a79354854adc86af0f4af3361074d
Shipu <242039583+shipukacapri@users.noreply.github.com>
2026-02-16 15:33:00 +0530

    derp maintainer

--- device.mk
+++ device.mk
@@ -51,6 +51,9 @@ PRODUCT_PACKAGES += \
     WifiResDeviceRetin \
     WifiResDeviceXT2083-4
 
+# Overlay Derp Maintainer
+PRODUCT_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.nfc.hce.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.hce.xml \
