1de8bbf9a906e0500a1bf4ad95b8fae5b57e72bc
Shipu <242039583+shipukacapri@users.noreply.github.com>
2026-02-20 08:25:55 +0530

    infinity bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,7 +1,7 @@
-#
-# SPDX-FileCopyrightText: The LineageOS Project
-# SPDX-License-Identifier: Apache-2.0
-#
-
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_guamp.mk
+    $(LOCAL_DIR)/infinity_guamp.mk
+
+COMMON_LUNCH_CHOICES := \
+    infinity_guamp-user \
+    infinity_guamp-userdebug \
+    infinity_guamp-eng
